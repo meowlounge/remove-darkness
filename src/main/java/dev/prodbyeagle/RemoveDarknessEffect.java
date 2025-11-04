53c64df3792c6cde0e47b32ce5b921cff6ff8ee9
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-11-04 19:11:34 +0100

    refactor: client side only mod. when server, then use the plugin.

--- src/main/java/dev/prodbyeagle/RemoveDarknessEffect.java
+++ src/main/java/dev/prodbyeagle/RemoveDarknessEffect.java
@@ -1,30 +1,36 @@
 package dev.prodbyeagle;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class RemoveDarknessEffect implements ModInitializer {
+public class RemoveDarknessEffect implements ClientModInitializer {
 	public static final String MOD_ID = "remove-darkness-effect-remastered";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("RemoveDarknessEffect mod initialized!");
+	private final DarknessEffectCleaner cleaner = new DarknessEffectCleaner();
 
-		ServerTickEvents.END_WORLD_TICK.register(world -> {
-			if (world instanceof ServerWorld serverWorld) {
-				for (ServerPlayerEntity player : serverWorld.getPlayers()) {
-					// check if player has the darkness effect, if yes remove that effect. that's it. lol.
-					if (player.hasStatusEffect(StatusEffects.DARKNESS)) {
-						player.removeStatusEffect(StatusEffects.DARKNESS);
-					}
+	@Override
+	public void onInitializeClient() {
+		LOGGER.info("RemoveDarknessEffect mod initialized!");
+		ServerTickEvents.END_SERVER_TICK.register(cleaner::onEndServerTick);
+	}
+
+	private static final class DarknessEffectCleaner {
+		void onEndServerTick(MinecraftServer server) {
+			if (!server.isSingleplayer()) {
+				return;
+			}
+
+			for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+				if (player.hasStatusEffect(StatusEffects.DARKNESS)) {
+					player.removeStatusEffect(StatusEffects.DARKNESS);
 				}
 			}
-		});
+		}
 	}
 }
